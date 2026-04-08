c52a2b3d6eb4896d2cdecba669a278f3009e5d22
bsc-27-24 <bsc-27-24@unima.ac.mw>
2026-04-08 16:40:40 +0300

    Add files via upload

---
+++ SwapNumbersByReference.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Function that swaps two integers using references
+void SwapNumbers(int &a, int &b) {
+int temp = a;
+a = b;
+b = temp;
+}
+
+int main() {
+int varA = 25;
+int varB = 100;
+
+cout << "varA before swap: " << varA << endl; // varA is 25
+cout << "varB before swap: " << varB << endl; // varB is 100
+
+SwapNumbers(varA, varB); // arguments passed by reference
+cout << "varA after swap: " << varA << endl; // varA is 100
+cout << "varB after swap: " << varB << endl; // varB is 25
+
+return 0;
+}
